ff14e8902d490404ce073e8384a9a5d2f89c5cea
Philippe Pepiot <phil@philpep.org>
2018-12-26 02:32:48 +0100

    dovecot: generate dh params
    This is required to have ssl working

--- dovecot/start.sh
+++ dovecot/start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
+if [ ! -s /etc/ssl/dh.pem ]; then
+    echo "Generating dh.pem"
+    openssl dhparam -out /etc/ssl/dh.pem 1024
+fi
 for fname in /etc/dovecot/dovecot-dict-quota.conf /etc/dovecot/dovecot-sql.conf; do
     sed -i "s/@@MAILSERVER_DB_USER@@/$MAILSERVER_DB_USER/g" $fname
     sed -i "s/@@MAILSERVER_DB_PASSWORD@@/$MAILSERVER_DB_PASSWORD/g" $fname
