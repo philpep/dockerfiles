952ab566562fb9c5e231e598b19daedfedc61e64
Philippe Pepiot <phil@philpep.org>
2019-05-06 14:23:23 +0200

    hgweb: switch to latest hg version and gunicorn
    * use latest mercurial with hg-evole
    * replace uwsgi by gunicorn
    * use pygments
    
    Based on https://bitbucket.org/snippets/av6/9Kd5M

--- hgweb/hgweb.py
+++ hgweb/hgweb.py
@@ -1,7 +1,6 @@
 config = "/etc/mercurial/hgweb.config"
 import os
 os.environ["HGENCODING"] = "UTF-8"
-import cgitb; cgitb.enable()
 from mercurial import demandimport; demandimport.enable()
 from mercurial.hgweb import hgweb
 application = hgweb(config)
