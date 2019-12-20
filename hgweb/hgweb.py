1891a307c5f5e0c717f24eda6337a12f9e6ff72d
Philippe Pepiot <phil@philpep.org>
2019-12-20 23:56:38 +0100

    hgweb: update to alpine 3.11 and python3

--- hgweb/hgweb.py
+++ hgweb/hgweb.py
@@ -1,6 +1,6 @@
-config = "/etc/mercurial/hgweb.config"
+config = b"/etc/mercurial/hgweb.config"
 import os
 os.environ["HGENCODING"] = "UTF-8"
-from mercurial import demandimport; demandimport.enable()
+import hgdemandimport; hgdemandimport.enable()
 from mercurial.hgweb import hgweb
 application = hgweb(config)
