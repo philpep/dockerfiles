1aad908ff47e2a0569d8ce7d6c41d439804279be
Philippe Pepiot <phil@philpep.org>
2020-03-14 17:33:55 +0100

    hgweb: workaround hgweb bug with demandimport
    See https://bz.mercurial-scm.org/show_bug.cgi?id=6268

--- hgweb/hgweb.py
+++ hgweb/hgweb.py
@@ -1,6 +1,6 @@
 config = b"/etc/mercurial/hgweb.config"
 import os
 os.environ["HGENCODING"] = "UTF-8"
-import hgdemandimport; hgdemandimport.enable()
+# import hgdemandimport; hgdemandimport.enable()
 from mercurial.hgweb import hgweb
 application = hgweb(config)
