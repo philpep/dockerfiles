c6257b1ad58a1d3bb71a05a83a82548cf12e9955
Philippe Pepiot <phil@philpep.org>
2020-10-16 22:12:46 +0200

    drop etherpad

--- etherpad/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /usr/share/etherpad-lite/src
-exec node /usr/share/etherpad-lite/src/node/server.js --settings /etc/etherpad-lite/settings.json --sessionkey /var/lib/etherpad-lite/SESSIONKEY.txt --credentials /var/lib/etherpad-lite/credentials.json --apikey /var/lib/etherpad-lite/APIKEY.txt
