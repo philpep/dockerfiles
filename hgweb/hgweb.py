cff06a6a4ed418d8f6a3d397aa80edd44138112e
Philippe Pepiot <phil@philpep.org>
2020-10-17 00:16:35 +0200

    hgweb: switch to debian buster

--- hgweb/hgweb.py
+++ hgweb/hgweb.py
@@ -1,6 +1,8 @@
-config = b"/etc/mercurial/hgweb.config"
 import os
-os.environ["HGENCODING"] = "UTF-8"
-# import hgdemandimport; hgdemandimport.enable()
 from mercurial.hgweb import hgweb
+config = b"/etc/mercurial/hgweb.config"
+os.environ["HGENCODING"] = "UTF-8"
+# demandimort is currently broken with python3:
+# https://bz.mercurial-scm.org/show_bug.cgi?id=6268
+# import hgdemandimport; hgdemandimport.enable()
 application = hgweb(config)
